2954246a1e74a47e68c2326326f6c77595fc085a
Anup Saud <anupsaud10@gmail.com>
2024-06-09 07:23:44 -0400

    Initial Commit

---
+++ BingeBot/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BingeBot
+//
+//  Created by Anup Saud on 2024-06-09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
